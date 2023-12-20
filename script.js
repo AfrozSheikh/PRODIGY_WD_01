80a8175f5750ce88b250ad58367beb9154dedc09
Afroz Sheikh <147607954+AfrozSheikh@users.noreply.github.com>
2023-12-20 12:20:49 +0000

    ok

--- script.js
+++ script.js
@@ -1,5 +1,81 @@
+const userTab = document.querySelector("[data-userWeather]") ; 
+const searchTab = document.querySelector("[data-searchedWeather]") ; 
+const userContainer = document.querySelector(".weather-container");
 
-   //API CALL
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-grantAccess]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
+
+// Initailly needed variable 
+
+
+let currentTab = userTab ; 
+const API_KEY = "9a1cddcf06f72d6c661d08d618e32176" ; 
+currentTab.classList.add("current-tab") ; 
+
+
+function switchTab(clickedTab){
+   if(clickedTab != currentTab){
+      currentTab.classList.remove("current-tab") ; 
+       currentTab = clickedTab ;
+       currentTab.classList.add("current-tab") ; 
+ 
+       if(!searchForm.classList.contains)
+   }
+}
+
+
+
+userTab.addEventListener('click' , ()=> 
+{
+   // pass clicked tab as input parameter 
+   switchTab(userTab) ; 
+})
+
+ searchTab.addEventListener('click',()=>{
+   switchTab(searchTab);
+ })
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //API CALL
    
 // async function t () { 
 //     let  city = "delhi" ; 
