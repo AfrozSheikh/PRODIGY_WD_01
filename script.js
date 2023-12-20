cd4cf8483e1bff204de84ca8082fdf24419a013b
Afroz Sheikh <147607954+AfrozSheikh@users.noreply.github.com>
2023-12-20 08:23:05 +0000

    STARTED

---
+++ script.js
@@ -0,0 +1,30 @@
+
+   //API CALL
+   
+// async function t () { 
+//     let  city = "delhi" ; 
+//     let key = "9a1cddcf06f72d6c661d08d618e32176"
+
+//     let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+//  let data = await result.json() ; 
+//  console.log(data);
+
+
+// }
+
+
+
+// TO PRINT LATTITUDE AND LONGITUDE
+
+// function getLoc() {
+//     if(navigator.geolocation) { 
+//         navigator.geolocation.getCurrentPosition(show) 
+
+//     }
+//     function show(position) {
+//         let lat = position.coords.latitude ; 
+//         let lon = position.coords.longitude ; 
+//         console.log(lat);
+//         console.log(lon);
+//     }
+// }
